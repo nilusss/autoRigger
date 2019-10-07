afd84cd70ee6a4fce453696069c5fcb3b77615e5
nilusss <48320346+nilusss@users.noreply.github.com>
2019-10-07 14:08:41 +0200

    update 229029292

--- code/python/autoRigger.py
+++ code/python/autoRigger.py
@@ -1,7 +1,22 @@
+import maya.cmds as mc
 import rigLib as rl
 print "ass"
 
 
-def build():
-    m = rl.base.module.Base(characterName='boi')
-    module = rl.base.module.Module(prefix='spine', baseObj=m)
+def UI():
+
+    if (mc.window("autoRigger", exists=True)):
+		mc.deleteUI("autoRigger")
+    
+    arWindow = mc.window("autoRigger", ex=False, title="big boi auto rig", iconName='big boi auto rig', widthHeight=(200, 55) )
+    arFormLayout = mc.formLayout()
+    arScrollLayout = mc.scrollLayout()
+    arColumnLayout = mc.columnLayout(adj=1)
+    arFrameLayout = mc.frameLayout(w=270, mw=10, cll=1, cl=1, l="Initial Setup")
+    mc.columnLayout(adj=0)
+    mc.button( label='Do Nothing' )
+    mc.button( label='Close', command=('cmds.deleteUI(\"' + arWindow + '\", window=True)') )
+    mc.setParent( '..' )
+    mc.showWindow( arWindow )
+    #m = rl.base.module.Base(characterName='boi')
+    #module = rl.base.module.Module(prefix='spine', baseObj=m)
